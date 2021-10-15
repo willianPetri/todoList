f07cd118d6cd88e48f389ac68efa3a2bc64c3f91
willian <wopetri11@gmail.com>
2021-10-14 20:56:59 -0400

    ListItem added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
- import * as C from './App.styles';
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
 
 const App = () => {
+  const[list, setList] = useState<Item[]>([
+    { id: 1, name: 'Compra pão na padaria', done: false},
+    { id: 2, name: 'Compra bolo na padaria', done: true},
+  ]);
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))} 
+
       </C.Area>
     </C.Container>
   );
