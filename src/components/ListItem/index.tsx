26b42b8a5ba410a2daf264cb2a954b0577fb8ba8
willian <wopetri11@gmail.com>
2021-10-14 21:43:36 -0400

    added AddArea and updated ListItem

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
 import * as C from './styles';
 import { Item } from '../../types/Item';
 
 type Props ={
   item: Item;
+  onChange: (id: number, done: boolean) => void
 }
 
-export const ListItem = ({ item }: Props) => {
-  const [isChecked, setIsChecked] = useState(item.done);  
-
+export const ListItem = ({ item, onChange }: Props) => {
   return(
-    <C.Container>
+    <C.Container done={item.done}>
       <input 
         type="checkbox" 
-        checked={isChecked} 
-        onChange={e => setIsChecked(e.target.checked)}
+        checked={item.done} 
+        onChange={e => onChange(item.id, e.target.checked)}
       />
       <label>{item.name}</label>
     </C.Container>
